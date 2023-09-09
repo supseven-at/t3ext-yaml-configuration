40c31643ff6ea09d93ecb1ac84d8e2889e0178b0
Josef Glatz <typo3@josefglatz.at>
2023-09-09 09:43:08 +0200

    [!!!][TASK] Remove support for TYPO3 10.5 LTS
    Support only two latest stable major
    versios to keep things simple

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-12.4.99',
+            'typo3' => '11.0.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
